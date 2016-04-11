35791a6ff0ab755e3f2143c19d7f6d09a908c0e8
Yunhong Jiang <yunhong.jiang@intel.com>
2016-04-11 12:10:14 -0700

    Need no proxy for local eth

--- proxy/onlyexport.sh
+++ proxy/onlyexport.sh
@@ -3,7 +3,7 @@
     export http_proxy=http://proxy-us.intel.com:911
     export https_proxy=https://proxy-us.intel.com:911
     export socks_proxy=http://proxy-us.intel.com:1080
-    export no_proxy=intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,127.0.0.0/8,134.134.0.0/16,172.16.0.0/20
+    export no_proxy=intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,127.0.0.0/8,134.134.0.0/16,172.16.0.0/20,172.25.0.0/16
     export GIT_PROXY_COMMAND=~/bin/socks-gw
     export PATH=~bin:$PATH
 
