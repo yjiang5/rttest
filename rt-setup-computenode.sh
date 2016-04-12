8d7e66fd6df80d5ce1d113f34c6020e4c626d87e
Yunhong Jiang <yunhong.jiang@intel.com>
2016-04-12 11:00:22 -0400

    The scripts to setup the compute node

---
+++ rt-setup-computenode.sh
@@ -0,0 +1,14 @@
+
+git config --global http.sslverify false
+
+# The followed part should be run on root account
+service libvirt-bin restart
+chmod a+rw /var/run/libvirt/libvirt-sock
+/usr/sbin/virtlogd -d
+chmod a+rw /var/run/libvirt/virtlogd-sock
+echo "12" > /sys/devices/system/node/node1/hugepages/hugepages-1048576kB/nr_hugepages
+echo "12" > /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages
+
+mkdir -p /mnt/hugetlbfs-1g
+sudo mount -t hugetlbfs hugetlbfs /mnt/hugetlbfs-1g
+
