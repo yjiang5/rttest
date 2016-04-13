bf9c885d2c9d319aa918ca9517a79fc6eee7447c
Cloud User <centos@frt.novalocal>
2016-04-13 01:21:43 +0000

    Run the basic fwd

--- guest.sh
+++ guest.sh
@@ -1,3 +1,9 @@
-examples/skeleton/build/basicfwd -l1 -b 00:03.0
+#!/bin/bash
 
+cd ~/source/dpdk/
+sudo modprobe uio
+sudo insmod x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
+sudo tools/dpdk_nic_bind.py -b igb_uio 00:05.0
 sudo tools/dpdk_nic_bind.py -b igb_uio 00:06.0
+
+sudo examples/skeleton/build/basicfwd -l1 -b 00:03.0
